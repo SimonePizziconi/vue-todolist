f9930f1d5960e2cea2fb5a7ef875ed8c0d2f8216
SimonePizziconi <simonepizziconi05@gmail.com>
2024-06-26 16:43:20 +0200

    feat: add line-through if done: true

--- js/main.js
+++ js/main.js
@@ -5,7 +5,27 @@ const { createApp } = Vue;
 createApp({
   data() {
     return {
-    
+      // Creiamo un array che contenga più oggetti
+      todo: [
+        {
+          text: "Fare i compiti",
+          done: true
+        },
+        {
+          text: "Mangiare",
+          done: false
+        },
+        {
+          text: "Studiare",
+          done: true
+        },
+        {
+          text: "Portare fuori il cane",
+          done: false
+        }
+      ]
+      
     }
-  }
+  },
+  
 }).mount('#app');
