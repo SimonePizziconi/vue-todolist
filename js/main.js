3e3e51de74b78266c9f1c8b80c24cc22c6e3542d
SimonePizziconi <simonepizziconi05@gmail.com>
2024-06-26 17:24:03 +0200

    feat: add new obj in todo with methods

--- js/main.js
+++ js/main.js
@@ -23,13 +23,25 @@ createApp({
           text: "Portare fuori il cane",
           done: false
         }
-      ]
-      
+      ],
+      newText: "",
     }
   },
   methods: {
+    
+    // Rimuovere item
     removeItem(i){
       this.todo.splice(i, 1);
-    }
-  }
+    },
+
+    // Aggiungere item
+    addItem(){
+      // Creare un nuovo item
+      let newItem = {text:this.newText, done:false};
+      this.todo.unshift(newItem);
+      this.newText = "";
+      console.log(this.todo);
+    },
+  }  
 }).mount('#app');
+
